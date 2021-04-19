d38ff9f82bc40d4006be78cb0f541e308403454d
Kupis.Piotr <222488@edu.p.lodz.pl>
2021-04-19 19:35:03 +0200

    Added test with sending zero rockets

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -38,5 +38,16 @@ class RadarTest {
         verify(batteryMock, times(rocketCount)).launchPatriot(enemyMissile);
     }
 
+    @RepeatedTest(5)
+    void setSendingZeroRocketsShouldInvokeLaunchPatriotZeroTimes() {
+
+        int rocketCount = 0;
+        BetterRadar radar = new BetterRadar(batteryMock, rocketCount, dummyExecutorService);
+        Scud enemyMissile = new Scud();
+        radar.notice(enemyMissile);
+
+        verify(batteryMock, times(rocketCount)).launchPatriot(enemyMissile);
+    }
+
 
 }
