94b79accb3d637f889b11dd9f3691e7f799aa16b
Kupis.Piotr <222488@edu.p.lodz.pl>
2021-04-19 19:32:36 +0200

    Added test with sending one rocket

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,24 +1,42 @@
 package edu.iis.mto.multithread;
 
-import static org.mockito.Mockito.verify;
-
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.concurrent.ExecutorService;
+
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class RadarTest {
 
     @Mock
     private PatriotBattery batteryMock;
+    @Mock
+    private ExecutorService dummyExecutorService;
 
-    @Test
-    void launchPatriotOnceWhenNoticesAScudMissle() {
-        Radar radar = new Radar(batteryMock);
-        Scud enemyMissle = new Scud();
-        radar.notice(enemyMissle);
-        verify(batteryMock).launchPatriot(enemyMissle);
+    @BeforeEach
+    void setup() {
+
+        when(dummyExecutorService.submit(any(Runnable.class))).thenAnswer(invocation -> {
+            ((Runnable) invocation.getArgument(0)).run();
+            return null;
+        });
     }
+
+    @RepeatedTest(5)
+    void setSendingOneRocketShouldInvokeLaunchPatriotOneTime() {
+
+        int rocketCount = 1;
+        BetterRadar radar = new BetterRadar(batteryMock, rocketCount, dummyExecutorService);
+        Scud enemyMissile = new Scud();
+        radar.notice(enemyMissile);
+
+        verify(batteryMock, times(rocketCount)).launchPatriot(enemyMissile);
+    }
+
 
 }
